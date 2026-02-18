46525bb465a9f5376f6590ca815ce6faf5d2735e
Lorenzoo195 <lorenzoactur@hotmail.com>
2026-02-18 20:38:08 +0100

    feat: restructure authentication routes and remove unused user router

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
 const cors = require('cors');
 const express = require('express');
-const userRouter = require('./route/users');
+const routes = require('./src/routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/users', userRouter);
+
+
+// api routes
+app.use('/api', routes);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(8080, () => {
   console.log("Iniciando el backend en el puerto 8080");
