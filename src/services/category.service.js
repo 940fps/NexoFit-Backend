b561f7e121897c6e49732521dc1dc37329f6efc9
Lorenzoo195 <lorenzoactur@hotmail.com>
2026-02-24 19:40:02 +0100

    feat: implement initial category service

---
+++ src/services/category.service.js
@@ -0,0 +1,12 @@
+/**
+ * @file Servicio que gestiona las consultas SQL de la tabla 'categories'.
+ * @module services/category
+ */
+
+const { db } = require("../config/database");
+
+
+async function findAllCategories() {
+
+    
+} 
